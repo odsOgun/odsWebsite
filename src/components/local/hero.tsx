9ea1fd4a4801b3f756bc1f09c08b641bf55f505a
Yusuf Kehinde <yusufkehinde11@gmail.com>
2024-08-13 12:15:25 +0100

    fix: updated readme

--- src/components/local/hero.tsx
+++ src/components/local/hero.tsx
@@ -10,12 +10,12 @@ const sponsors = [
   { label: 'Afex', icon: AfexLogo },
   { label: 'Ogun Gov', icon: OgunGov },
   { label: 'Tech Economy', icon: TechEconomy },
-  { label: 'Ogun Daily', icon: OgunDaily },
+  { label: 'Ogun Daily', icon: OgunDaily }
 ];
 
 function SponsorLogos() {
   return (
-    <div className="flex items-center justify-between px-6 mt-5 h-[72px]">
+    <div className='flex items-center justify-between px-6 mt-5 h-[72px]'>
       {sponsors.map(({ label, icon }) => (
         <div key={label}>
           <img src={icon} alt={label} />
@@ -27,42 +27,39 @@ function SponsorLogos() {
 
 function Hero() {
   return (
-    <div className="hero-bg pb-[76px]">
-      <div className="max-w-[713px] mx-auto pt-[100px]">
-        <div className="flex flex-col gap-8">
-          <div className="text-6xl leading-[64px] text-center tracking-[-1px] text-[#23323F]">
-            <p className="italic font-normal platypi-gf">
+    <div className='hero-bg pb-[76px] '>
+      <div className='max-w-[713px] mx-auto pt-[100px]'>
+        <div className='flex flex-col gap-8'>
+          <div className='text-6xl leading-[64px] text-center tracking-[-1px] text-[#23323F]'>
+            <p className='italic font-normal platypi-gf'>
               Innovation:{' '}
-              <span className="font-semibold tracking-[-1px] not-italic">
+              <span className='font-semibold tracking-[-1px] not-italic'>
                 Pathway to our nation's prosperity
               </span>
             </p>
           </div>
-          <p className="text-base font-normal tracking-[0.2px] text-[#627587] text-center">
-            Ogun Digital Summit 2024 is focused on improving collaborations,
-            paving the way for new innovations and igniting conversation towards
-            technology advancements and prosperity for Nigeria's digital
-            technology ecosystem.
+          <p className='text-base font-normal tracking-[0.2px] text-[#627587] text-center'>
+            Ogun Digital Summit 2024 is focused on improving collaborations, paving the way for new
+            innovations and igniting conversation towards technology advancements and prosperity for
+            Nigeria's digital technology ecosystem.
           </p>
 
-          <div className="flex items-center justify-center gap-4">
-            <button className="bg-[#178A2D] font-semibold h-10 min-w-[110px] rounded flex justify-center items-center tracking-[0.2px] text-white">
-              <span className="text-sm font-semibold">Register</span>
+          <div className='flex items-center justify-center gap-4'>
+            <button className='bg-[#178A2D] font-semibold h-10 min-w-[110px] rounded flex justify-center items-center tracking-[0.2px] text-white'>
+              <span className='text-sm font-semibold'>Register</span>
               <ArrowRight />
             </button>
-            <button className="min-w-[161px] rounded-[2px] h-6 bg-white flex justify-center items-center gap-2">
-              <span className="text-[#178A2D] text-sm font-semibold">
-                Become a sponsor
-              </span>
+            <button className='min-w-[161px] rounded-[2px] h-6 bg-white flex justify-center items-center gap-2'>
+              <span className='text-[#178A2D] text-sm font-semibold'>Become a sponsor</span>
               <ArrowRightGreen />
             </button>
           </div>
         </div>
       </div>
 
-      <div className="max-w-[713px] mx-auto pt-[100px]">
-        <div className="flex flex-col justify-center w-full">
-          <p className="text-center font-semibold text-base uppercase text-[#627587] tracking-[3px]">
+      <div className='max-w-[713px] mx-auto pt-[100px]'>
+        <div className='flex flex-col justify-center w-full'>
+          <p className='text-center font-semibold text-base uppercase text-[#627587] tracking-[3px]'>
             Proudly supported by
           </p>
           <SponsorLogos />
