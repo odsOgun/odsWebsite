0385d020582973c53d9ac2aadfa7ff21af21ab23
seunbayo <seunbayo83@gmail.com>
2024-08-23 12:38:57 +0100

    fix(footer): fix social icon link

--- src/components/local/footer.tsx
+++ src/components/local/footer.tsx
@@ -2,25 +2,27 @@ import ODSlogo from '@/assets/img/ODSlogo.png';
 import instagram from '@/assets/img/instagram.png';
 import twitter from '@/assets/img/twitter.png';
 import gmail from '@/assets/img/gmail.png';
-import Sitelinks from '@/mock/sitelinks.json';
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div className='bg-[#101611] '>
+    <div className='bg-[#101611]'>
       <div className='art-bg' />
-      <div className=' max-w-[1120px] mx-auto my-0 w-full p-20 px-8 xl:box-content  '>
-        <div className='flex justify-center align-center '>
+      <div className='max-w-[1120px] mx-auto my-0 w-full p-20 px-8 xl:box-content'>
+        <div className='flex justify-center align-center'>
           <img src={ODSlogo} alt='ogundigitalsummitlogo' />
         </div>
 
-        <div className='flex  justify-center align-center text-base font-semibold  text-[#B0C5D6] pt-[48px] list-none gap-10 md:gap-16 leading-6 max-md:flex-col max-md:items-center  '>
+        <div className='flex justify-center align-center text-base font-semibold text-[#B0C5D6] pt-[48px] list-none gap-10 md:gap-16 leading-6 max-md:flex-col max-md:items-center'>
           <li>About</li>
           <li>
-            <a href='https://airtable.com/appyfwCgYP4N5vYUY/pagFPHzIMU2TSypBt/form' target='_blank'>
+            <a
+              href='https://airtable.com/appyfwCgYP4N5vYUY/pagFPHzIMU2TSypBt/form'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               Sponsors
             </a>
           </li>
-
           <li>
             <a
               href='https://ogundigitalsummit.bumpa.shop'
@@ -30,29 +32,36 @@ const footer = () => {
               Store
             </a>
           </li>
-
           <li>
-            <a href='https://airtable.com/appyfwCgYP4N5vYUY/pagn6mVmzmgXH8ZVR/form' target='_blank'>
+            <a
+              href='https://airtable.com/appyfwCgYP4N5vYUY/pagn6mVmzmgXH8ZVR/form'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               Exhibitors
             </a>
           </li>
         </div>
 
-        <div className=' flex  justify-around align-center mt-12 md:mt-[64px] max-md:flex-col  max-md:items-center max-md:gap-12 '>
-          <div className='flex justify-center align-center gap-8   '>
-            <a href={Sitelinks.instagram} target='_blank'>
-              <img src={instagram} alt='instagramlogo' />
+        <div className='flex justify-around align-center mt-12 md:mt-[64px] max-md:flex-col max-md:items-center max-md:gap-12'>
+          <div className='flex justify-center align-center gap-8'>
+            <a
+              href='https://www.instagram.com/ogundgtsummit'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src={instagram} alt='instagram logo' />
             </a>
-            <a href={Sitelinks.twitter} target='_blank'>
-              <img src={twitter} alt='twitterlogo' />
+            <a href='https://www.x.com/ogundgtsummit' target='_blank' rel='noopener noreferrer'>
+              <img src={twitter} alt='twitter logo' />
             </a>
-            <a href={Sitelinks.email} target='_blank'>
-              <img src={gmail} alt='gmaillogo' />
+            <a href='mailto:hello@ogundigitalsummit.com' target='_blank' rel='noopener noreferrer'>
+              <img src={gmail} alt='gmail logo' />
             </a>
           </div>
 
           <div className='text-[#627587] text-center text-xs md:text-base leading-[18px] md:leading-6 font-normal tracking-[-0.2px] md:tracking-[0.2px]'>
-            <p>Copyright. 2024 Ogun Digital Summit. All rights reserved</p>
+            <p>Copyright © 2024 Ogun Digital Summit. All rights reserved.</p>
           </div>
         </div>
       </div>
@@ -60,4 +69,4 @@ const footer = () => {
   );
 };
 
-export default footer;
+export default Footer;
